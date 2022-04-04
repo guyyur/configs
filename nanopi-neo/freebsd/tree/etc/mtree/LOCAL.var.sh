13d756dec02460ccf1bb5e1d86f52620744ac0f6
Guy Yur <guyyur@gmail.com>
2022-04-04 22:12:13 +0300

    sync with private configs

--- nanopi-neo/freebsd/tree/etc/mtree/LOCAL.var.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/usr/bin/install -d -m 0755 -o root -g wheel /var/db/dhcpcd
-/usr/bin/install -c -m 0644 -o root -g wheel /etc/local/dhcpcd.duid.backup /var/db/dhcpcd/duid
-/usr/bin/install -c -m 0600 -o root -g wheel /etc/local/dhcpcd.secret.backup /var/db/dhcpcd/secret
-/usr/bin/install -d -m 0700 -o guy -g guy /var/tmp/guy
-/usr/bin/install -d -m 0755 -o root -g wheel /var/xdg-cache
-/usr/bin/install -d -m 0700 -o guy -g guy /var/xdg-cache/guy
